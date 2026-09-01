65dedc2040367d6f12d1587c72db68a52eb8faf7
Mr Punk da Silva <gustavojs417@gmail.com>
2026-09-01 16:01:01 -0300

    fix(profile): add type assertions for discriminated union spread in switch cases

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -5,19 +5,25 @@ import { ProfileView } from "./views/profile-view/profile-view.view";
 import { ProfileEditView } from "./views/profile-edit/profile-edit.view";
 import { ProfilePasswordView } from "./views/profile-password/profile-password.view";
 import { ProfileLoadingView } from "./views/profile-loading/profile-loading.view";
+import type {
+  ProfileLoadingViewProps,
+  ProfileEditViewProps,
+  ProfilePasswordViewProps,
+  ProfileViewProps,
+} from "./types/profile.types";
 
 export default function ProfilePage() {
   const result = useProfilePage();
 
   switch (result.view) {
     case "loading":
-      return <ProfileLoadingView {...result.props} />;
+      return <ProfileLoadingView {...result.props as ProfileLoadingViewProps} />;
     case "edit":
-      return <ProfileEditView {...result.props} />;
+      return <ProfileEditView {...result.props as ProfileEditViewProps} />;
     case "password":
-      return <ProfilePasswordView {...result.props} />;
+      return <ProfilePasswordView {...result.props as ProfilePasswordViewProps} />;
     case "view":
-      return <ProfileView {...result.props} />;
+      return <ProfileView {...result.props as ProfileViewProps} />;
     default:
       return null;
   }
